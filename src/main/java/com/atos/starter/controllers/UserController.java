c29028211c68293a5c8200be39e5fd2edf261fd2
ach-raf <achraf.zouchou@gmail.com>
2021-03-30 10:18:40 +0100

    -added userService

--- src/main/java/com/atos/starter/controllers/UserController.java
+++ src/main/java/com/atos/starter/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.atos.starter.controllers;
 
 import com.atos.starter.dao.UserRepository;
 import com.atos.starter.models.User;
+import com.atos.starter.services.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,28 +18,26 @@ import java.util.Optional;
 @RequestMapping("/api")
 // Accept Cross origin calls
 @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5000"})
+@AllArgsConstructor
 public class UserController {
-	private final UserRepository user_repo;
-	
-	public UserController(UserRepository user_repo) {
-		this.user_repo = user_repo;
-	}
+	private final UserService userService;
+
 	// Get a list of all users
 	@GetMapping("/users")
     Collection<User> Users() {
-        return (List<User>) user_repo.findAll();
+        return userService.findAll();
     }
 	// Get a single user by id
     @GetMapping("/user/{id}")
     ResponseEntity<?> getGroup(@PathVariable String id) {
-        Optional<User> user = user_repo.findById(id);
+        Optional<User> user = userService.findById(id);
         return user.map(response -> ResponseEntity.ok().body(response))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
     // Update user method
     @PutMapping("/user/{id}")
     ResponseEntity<User> updateUser(@RequestBody User user) {
-        User result = user_repo.save(user);
+        User result = userService.add(user);
         return ResponseEntity.ok().body(result);
     }
 
