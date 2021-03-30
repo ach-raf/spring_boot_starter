18b4338c094eea9dc01a808a7066ba9ecd52e07b
ach-raf <achraf.zouchou@gmail.com>
2021-03-30 10:28:15 +0100

    -added userService

---
+++ src/main/java/com/atos/starter/services/UserService.java
@@ -0,0 +1,40 @@
+package com.atos.starter.services;
+
+import com.atos.starter.dao.UserRepository;
+import com.atos.starter.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+
+public class UserService {
+    @Autowired
+    private UserRepository user_repository;
+
+    public List<User> findAll() {
+        return (List<User>) user_repository.findAll();
+    }
+
+    public Optional<User> findById(String _id) {
+        return user_repository.findById(_id);
+    }
+
+    public void deleteById(String _id) {
+        user_repository.deleteById(_id);
+    }
+
+    public User add(User _user) {
+        return user_repository.save(_user);
+    }
+
+    public User update(String _id_to_update, User _user) {
+        _user.setId(_id_to_update);
+        return user_repository.save(_user);
+    }
+
+
+
+}
